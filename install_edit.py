aea1854a4498eb9efa58f12e4005ccebb5fff035
AdroitBit <yanothaic15@gmail.com>
2024-04-13 15:45:58 +0700

    break system packages

--- install_edit.py
+++ install_edit.py
@@ -1,14 +1,14 @@
 import os
 import glob
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-os.system("pip install -r requirements.txt")
+os.system("pip install -r requirements.txt --break-system-packages")
 print("="*20)
 
 try:
     import obeyon_rfs
-    os.system("pip uninstall obeyon_rfs -y")
+    os.system("pip uninstall obeyon_rfs -y --break-system-packages")
     print("="*20)
 except ImportError:
     pass
 
-os.system("pip install -e .")
+os.system("pip install -e . --break-system-packages")
